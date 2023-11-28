d4c748780f0f033311030149568e7b88e39de2ac
Gustavo00101 <150172214+Gustavo00101@users.noreply.github.com>
2023-11-28 16:51:58 -0300

    atividade2.c

--- atividade2.c
+++ atividade2.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-#include <string.h>
+
 
 
 int main () {
-    setlocale(LC_ALL,"portuguese");
+    
 
     int linha = 5;
     int coluna = 3;
